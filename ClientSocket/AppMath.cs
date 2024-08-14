cf8af769e6d5954bd41891a98d13d623798021fd
maiTheLong <longdevunity@gmail.com>
2024-08-14 17:59:02 +0700

    -update appMath and data tranfer

---
+++ ClientSocket/AppMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSocket
+{
+	public class AppMath
+	{
+		public static void ConvertStructeToByteArr<T>(T structure,ref int size, ref byte[] output)
+		{
+			size= Marshal.SizeOf(structure);
+			if(size<=0) return;	
+			IntPtr ptr = Marshal.AllocHGlobal(size);
+			Marshal.StructureToPtr(structure, ptr, false);
+			Marshal.Copy(ptr, output, 0, size);
+			Marshal.FreeHGlobal(ptr);
+		}
+		public static void ConvertByteArrToStructure<T>(byte[] input, ref int size,ref T output)
+		{
+			size = input.Length;
+			if (size <= 0) return;
+			IntPtr ptr = Marshal.AllocHGlobal(size);
+			Marshal.Copy(input,0,ptr, size);
+			Marshal.PtrToStructure(ptr, output);
+			Marshal.FreeHGlobal(ptr);
+		}
+	}
+}
---
+++ ClientSocket/ST_DATA_TRANFER.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSocket
+{
+	[StructLayout(LayoutKind.Sequential,Pack =1)]
+	public struct ST_DATA_TRANFER
+	{
+		public uint DataInt;
+		public ushort DataUshort;
+		[MarshalAs(UnmanagedType.ByValTStr,SizeConst =10)]
+		public string DataString;
+		public byte[] DataByteArr;
+		public bool DataBool;
+	}
+}
