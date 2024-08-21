8c941911bb390ed9b93bff4f125fec4bcd9fd9fe
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2024-08-21 23:46:08 +0700

    - adding client manager

--- ServerSocket/Server.cs
+++ ServerSocket/Server.cs
@@ -13,15 +13,16 @@ namespace ServerSocket
 	public class Server
 	{
 		public  static Socket _listener;
-		public static Socket client;
 		public static IPAddress _address;
 		public static IPEndPoint _endPoint;
 		public static int MaxUser;
 		public static string Hello="Hello from server";
+		public static List<Socket> allClient;
 		public static void Start(string host,int port,int maxUser)
 		{
 			MaxUser = maxUser;
-			_address= IPAddress.Parse(host);
+			allClient= new List<Socket> ();
+			_address = IPAddress.Parse(host);
 			_endPoint= new IPEndPoint(_address, port);
 			_listener= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);	
 			_listener.Bind(_endPoint);
@@ -36,12 +37,14 @@ namespace ServerSocket
 		{
 			try
 			{
-				 client= _listener.EndAccept(ar);
+				Socket client = _listener.EndAccept(ar);
+				allClient.Add(client);
 				Console.WriteLine($" client {client.RemoteEndPoint} connected to server ....");
 				_listener.BeginAccept(OnAcceptCLient, null);
-				ST_DATA_TRANFER sT_DATA_TRANFER= default( ST_DATA_TRANFER );
-				sT_DATA_TRANFER.DataString = Hello;
-				Send(sT_DATA_TRANFER);
+				ST_DATA_TRANFER sT_DATA_TRANFER = default(ST_DATA_TRANFER);
+				sT_DATA_TRANFER.DataString = $"new client enter server with endpoint {client.RemoteEndPoint}";
+				//Send(sT_DATA_TRANFER);
+				SendBroadcast(sT_DATA_TRANFER);
 				byte[] buffer = new byte[1024];
 				client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, OnReceiveFromClient, client);
 			
@@ -55,44 +58,96 @@ namespace ServerSocket
 				Stop();
 			}
 		}
+
+		private static void OnReceiveFromClient(IAsyncResult ar)
+		{
+
+			Socket client = (Socket)ar.AsyncState;
+			int byteRead = 0;
+			try
+			{
+				byteRead = client.EndReceive(ar);
+				if(byteRead<=0)
+				{
+					client.Close();
+					allClient.Remove(client);
+					Console.WriteLine($" client {client.RemoteEndPoint} disconnect from server");
+					return;
+				}
+				SendBroadcast(new ST_DATA_TRANFER()
+				{
+					DataInt = (uint)allClient.IndexOf(client),
+					DataString = $"client  {client.RemoteEndPoint} connected to server",
+					DataBool = true,
+					DataByteArr = Encoding.UTF8.GetBytes($"client  {client.RemoteEndPoint} connected to server"),
+					DataUshort = (ushort)byteRead
+				});
+
+			}
+			catch (SocketException ex) { 
+				client.Close();
+				allClient.Remove(client);
+				Console.WriteLine($" client wrong disconnect due to {ex.ToString()}" );
+			}
+		}
+
 		//private static void Send(string message) { 
 		//	byte[] buffer = Encoding.UTF8.GetBytes(message);
 		//	client.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, OnSendDataToCLient, null);
 		//}
-		private static void Send(object data)
+		private static void Send(object data,Socket client)
 		{
 			byte[] output = new byte[1024];
 			int size = 0;
 			AppMath.ConvertStructeToByteArr(data, ref size, ref output);
-			client.BeginSend(output, 0, output.Length, SocketFlags.None, OnSendDataToCLient, null);
-		
+			client.BeginSend(output, 0, output.Length, SocketFlags.None, OnSendDataToCLient, new SendData() {
+				sk=client,
+				data=output
+			});
+		}
+		private static void SendBroadcast(object data)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine($" all client ");
+			foreach (var client in allClient)
+			{
+				if (client!=null&& client.Connected)
+				{
+					stringBuilder.AppendLine($" client {allClient.IndexOf(client)} with endpoint {client.RemoteEndPoint}");
+					Send(data, client);
+				}
+				else
+				{
+					//allClient.Remove(client);
+				}
+			}
+			Console.WriteLine(stringBuilder.ToString());
 		}
 
 		private static void OnSendDataToCLient(IAsyncResult ar)
 		{
-			var byteSend= client.EndSend(ar);
-			if(byteSend>0)
-			{
-				Console.WriteLine($"Send data success to client with hello {Hello}");
-			}
-		}
-
-		private static void OnReceiveFromClient(IAsyncResult ar)
-		{
+			SendData sendData = (SendData)ar.AsyncState;
+			Socket socket = sendData.sk;
+			byte[] data = sendData.data;
 			try
 			{
-				Socket socket = (Socket)ar.AsyncState;
-				if(socket!=client)
+				var byteSend = socket.EndSend(ar);
+				if (byteSend > 0)
 				{
-					Console.WriteLine($" socket receive khac socket base");
-					return;
+					ST_DATA_TRANFER sT_DATA_TRANFER= default(ST_DATA_TRANFER);
+					AppMath.ConvertByteArrToStructure(data, byteSend, ref sT_DATA_TRANFER);
+					Console.WriteLine($"Send data success to client with {sT_DATA_TRANFER.ToString()} ");
 				}
-				int byteRead=client.EndReceive(ar);
-				if (byteRead > 0) {
-					Console.WriteLine($"receive data success");
-				}
-			}catch(Exception ex) { Console.WriteLine(ex.ToString()); }
+			}catch(SocketException ex)
+			{
+				socket.Close();
+				allClient.Remove(socket);
+				Console.WriteLine($"cant send data to server due to {ex.Message}");
+			}
+			
 		}
+
+		
 
 		public static void Stop() { 
 			_listener.Shutdown(SocketShutdown.Both);
@@ -102,4 +157,9 @@ namespace ServerSocket
 		}
 		
 	}
+}
+public  class SendData
+{
+	public Socket sk;
+	public byte[] data;
 }
